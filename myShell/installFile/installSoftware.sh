0a7c1e07a24d842a0b758fcf7a649fb869dd02c7
zj <zhangjian0395@163.com>
2017-09-10 23:36:24 +0800

    安装

--- myShell/installFile/installSoftware.sh
+++ myShell/installFile/installSoftware.sh
@@ -12,7 +12,7 @@ sudo apt-get remove libreoffice-common unity-webapps-common thunderbird totem rh
 sudo apt autoremove
 
 #把源设置为阿里云源
-sudo ./installFile/setSource.sh
+sudo ./sources/setSource.sh
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get dist-upgrade
@@ -58,6 +58,10 @@ sudo apt-get install lantern
 sudo apt-get install wps-office
 #安装autojump
 git clone git://github.com/joelthelion/autojump.git $installPath/autojump && cd $installPath/autojump && ./install.py
+#终端的颜色表
+wget -O xt  http://git.io/v3Dll && chmod +x xt && ./xt && rm xt
 #安装oh-my-git
 git clone https://github.com/arialdomartini/oh-my-git.git ~/.oh-my-git && echo source ~/.oh-my-git/prompt.sh >> ~/.bashrc
-
+#oh-my-git需要的字体，需手动设置终端字体为SourceCodePro+Powerline+Awesome Regular
+cd /tmp && git clone http://github.com/gabrielelana/awesome-terminal-fonts && cd awesome-terminal-fonts && git checkout patching-strategy && mkdir -p ~/.fonts && cp patched/*.ttf ~/.fonts
+sudo fc-cache -fv ~/.fonts
--- myShell/installFile/sources/setSource.sh
+++ myShell/installFile/sources/setSource.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ubuntuVersion=`cat /etc/issue | grep -o "[0-9]*\.[0-9]*"`
-sudo cp "sources/$ubuntuVersion" /etc/apt/sources.list
+sudo cp "$ubuntuVersion" /etc/apt/sources.list
