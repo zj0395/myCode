3738a4806c6caef1a41dd4a15386471227e98683
zhangjian <zhangjian0395@163.com>
2018-01-27 12:12:22 +0800

    修改默认源

--- myShell/installFile/sources/setSource.sh
+++ myShell/installFile/sources/setSource.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #ubuntuVersion=`cat /etc/issue | grep -o "[0-9]*\.[0-9]*"`
-echo `pwd`
-ubuntuVersion='tsinghua'
-sudo cp "$ubuntuVersion" /etc/apt/sources.list
+sourceChoose='aliyun'
+sudo cp "$sourceChoose" /etc/apt/sources.list
