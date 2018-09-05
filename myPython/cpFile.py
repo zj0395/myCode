cceea2f19d3aa06bb7a6b398ee5bb068d47ea3ea
zhangjian <zhangjian0395@163.com>
2018-09-05 22:16:31 +0800

    完善文件转二维码的功能，删除无用文件

--- myPython/cpFile.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding=utf-8
-from sys import argv
-fromFile = 'loging.py'
-destFile = "dest.txt"
-inFile = open(fromFile)
-inData = inFile.read()
-outFile = open(destFile, 'w')
-outFile = outFile.write(inData)
--- myPython/file2qr.py
+++ myPython/file2qr.py
@@ -1,28 +1,97 @@
-#-*- Coding: UTF-8 -*-
+#-*- coding: UTF-8 -*-
 __author = 'zj'
 
-import base64, sys, qrcode
+import base64, os, sys, qrcode, math
 from PIL import Image
 from pyzbar import pyzbar
 
+TEST_FILE = './file2qr.py_1.png'
+SPLIT_SYMBOL_FINAL = b'____'
+SPLIT_SYMBOL = b'__'
+
+
+class FileManager(object):
+    class OneFile(object):
+        def __init__(self, name):
+            self._name = 'temp'+name
+            self._count = 0
+            self._list = []
+            self._result = []
+
+        @property
+        def count(self):
+            return self._count
+        @count.setter
+        def count(self, count):
+            self._count=count
+            self._list = [None]*count
+            self._result = [None]*count
+
+        def addIdx(self, idx, result):
+            self._list[idx] = 1
+            self._result[idx] = result
+            if None not in self._list:
+                self._save()
+
+        def _save(self):
+            result = b''.join(self._result)
+            print(len(result), result)
+            f = open(self._name, 'wb')
+            f.write(result)
+            f.close()
+
+    _map = {}
+
+    def __init__(self):
+        pass
+
+    def addFile(self, name, idx, count, result):
+        if name not in self._map:
+            newFile = self.OneFile(name)
+            newFile.count = count
+            self._map[name] = newFile
+        self._map[name].addIdx(idx, result)
+
+
+def encodeName(name, idx, count):
+    ret = b'%s%s%d%s%d%s' %(name.encode(), SPLIT_SYMBOL, idx, SPLIT_SYMBOL, count, SPLIT_SYMBOL_FINAL)
+    return ret
+
+def decodeName(str):
+    countIdx = str.find(SPLIT_SYMBOL_FINAL)
+    result = str[ countIdx+len(SPLIT_SYMBOL_FINAL) : ]
+    str = str[:countIdx]
+    countIdx = str.rfind(SPLIT_SYMBOL)
+    count = str[ countIdx+len(SPLIT_SYMBOL): ]
+    str = str[:countIdx]
+    countIdx = str.rfind(SPLIT_SYMBOL)
+    idx = str[ countIdx+len(SPLIT_SYMBOL): ]
+    name = str[:countIdx]
+    return name.decode(), int(idx), int(count), result
+
+
 def file2QrCode(file):
-    MAX_LENGTH = 2300 # 最大为2331
+    # 一个二维码最多保存2331字节，适当减少
+    MAX_LENGTH = 1024
     f = open(file, 'rb')
     code = f.read()
     f.close()
-    # code = base64.b64encode(code)
+    print(len(code),code)
     idx = 0
+    filePath, fileName = os.path.split(file)
     fileList = []
-    while idx < len(code):
-        oneCode = code[idx:idx+MAX_LENGTH]
-        # qr = qrcode.QRCode()
-        # qr.add_data(oneCode)
-        # qr.make()
-        # oneImg=qr.make_image()
-        oneImg = qrcode.make(oneCode)
-        onePath = '%s%d.png' %(file, idx/MAX_LENGTH+1)
+    fileSplitNum = math.ceil(len(code)/MAX_LENGTH)
+    while idx < fileSplitNum:
+        oneCode = encodeName(fileName, idx, fileSplitNum) + code[idx*MAX_LENGTH:(idx+1)*MAX_LENGTH]
+        oneCode = base64.b64encode(oneCode)
+        qr = qrcode.QRCode()
+        qr.add_data(oneCode)
+        qr.make()
+        oneImg=qr.make_image()
+        # oneImg = qrcode.make(oneCode)
+        onePath = '%s_%d.png' %(file, idx+1)
         oneImg.save( onePath )
-        idx = idx + MAX_LENGTH
+        idx = idx + 1
         fileList.append(onePath)
     return fileList
 
@@ -32,13 +101,11 @@ def qr2file(img):
     return ret[0].data
 
 def __test__():
-    result = b''
-    for file in file2QrCode( 'test.py' ):
-        result = result + qr2file( file )
-    # result = base64.b64decode(result)
-    f = open('test_result.py', 'wb')
-    f.write(result)
-    f.close()
+    fmanager = FileManager()
+    for file in file2QrCode( TEST_FILE ):
+        oneResult = qr2file( file )
+        oneResult = base64.b64decode(oneResult)
+        fmanager.addFile( *decodeName(oneResult) )
 
 
 if __name__ == '__main__':
